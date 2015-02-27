609c8841112a88cd92ec81338eedae40cc953415
SFoskitt <stephanie.foskitt@gmail.com>
2015-02-26 19:32:06 -0600

    getElements... completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+	var ret = [];
+	var recurse = function(elem){
+		var elems = elem.children;
+		for (var i = 0; i < elems.length; i++){
+			if(elems[i].classList.contains(className)) { ret.push(elems[i]) };
+			if(elems[i].children.length > 0) { recurse(elems[i]) };
+		};
+	};
+	recurse(document);
+	return ret;
 };
